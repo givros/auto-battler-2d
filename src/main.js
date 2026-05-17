17da41d51987e72371b9d0441a382f9b7b662c0f
Givros <69457849+givros@users.noreply.github.com>
2026-05-17 21:03:20 +0200

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-(function (AB) {
-  window.addEventListener("DOMContentLoaded", async () => {
-    const assets = await new AB.AssetLoader().load();
-    const game = new AB.Game(assets);
-    window.autoBattlerGame = game;
-    game.start();
-  });
-})(window.AutoBattler = window.AutoBattler || {});
